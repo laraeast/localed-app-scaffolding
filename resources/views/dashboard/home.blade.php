0a08d6c69fdd10cfe5ab169d05e03918a69a9141
Ahmed Fathy <aliraqi.dev@gmail.com>
2019-05-05 17:59:33 +0200

    add home page

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -1,1 +1,49 @@
 @extends('layouts.dashboard')
+
+@section('content')
+
+    @component('adminlte::page', ['title' => trans('dashboard.home'), 'breadcrumb' => 'dashboard.home'])
+
+        <div class="row">
+
+            <div class="col-md-3">
+                @include('adminlte::info-box', [
+                    'icon_color' => 'green',
+                    'icon' => 'user-secret',
+                    'text' => 'Admins',
+                    'number' => '1',
+                ])
+            </div>
+
+            <div class="col-md-3">
+                @include('adminlte::info-box', [
+                    'icon_color' => 'red',
+                    'icon' => 'users',
+                    'text' => 'Users',
+                    'number' => '3',
+                ])
+            </div>
+
+            <div class="col-md-3">
+                @include('adminlte::info-box', [
+                    'icon_color' => 'yellow',
+                    'icon' => 'list',
+                    'text' => 'Categories',
+                    'number' => '3',
+                ])
+            </div>
+
+            <div class="col-md-3">
+                @include('adminlte::info-box', [
+                    'icon_color' => 'blue',
+                    'icon' => 'envelope-o',
+                    'text' => 'Unread Messages',
+                    'number' => '5',
+                ])
+            </div>
+
+        </div>
+
+    @endcomponent
+
+@endsection
