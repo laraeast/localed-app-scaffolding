de9df4f7a4131261b47e267e6cfe7b14f828f312
Ahmed Fathy <aliraqi.dev@gmail.com>
2019-08-20 07:17:36 +0200

    remove unused keys

--- resources/lang/ar/web.php
+++ resources/lang/ar/web.php
@@ -1,20 +1,8 @@
 <?php
 
 return [
-    'important-links' => 'روابط هامة',
-    'about-company' => 'نبذة عن الشركة',
-    'our-products' => 'منتجاتنا',
-    'services' => 'خدماتنا',
-    'customer-opinions' => 'آراء العملاء',
-    'work-team' => 'فريق العمل',
     'menus' => [
         'home' => 'الرئيسية',
-        'about' => 'من نحن',
-        'products' => 'المنتجات',
         'feedback' => 'اتصل بنا',
-    ],
-    'newsletter' => [
-        'placeholder' => 'ادخل البريد الالكتروني',
-        'submit' => 'إشتراك',
     ],
 ];
--- resources/lang/en/web.php
+++ resources/lang/en/web.php
@@ -1,20 +1,8 @@
 <?php
 
 return [
-    'important-links' => 'Important Links',
-    'about-company' => 'About Company',
-    'our-products' => 'Our Products',
-    'services' => 'Our Services',
-    'customer-opinions' => 'Customer Opinions',
-    'work-team' => 'Work Team',
     'menus' => [
         'home' => 'Home',
-        'about' => 'About Us',
-        'products' => 'Products',
         'feedback' => 'Contact Us',
-    ],
-    'newsletter' => [
-        'placeholder' => 'Enter your E-mail',
-        'submit' => 'Subscribe',
     ],
 ];
